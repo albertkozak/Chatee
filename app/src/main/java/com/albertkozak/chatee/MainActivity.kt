1045411bb58e51bd603c883788d9ca07d8dc54bf
Albert Kozak <albert.kozak@live.com>
2020-02-20 10:48:28 -0800

    Added login screen / authentication

--- app/src/main/java/com/albertkozak/chatee/MainActivity.kt
+++ app/src/main/java/com/albertkozak/chatee/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.albertkozak.chatee
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login_button.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
 
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("message")
