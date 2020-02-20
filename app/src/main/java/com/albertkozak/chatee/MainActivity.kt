eaeb80192cfe3dd63a29c6e12d77f391c3f70eba
Albert Kozak <albert.kozak@live.com>
2020-02-20 10:31:50 -0800

    Added firebase test connection

--- app/src/main/java/com/albertkozak/chatee/MainActivity.kt
+++ app/src/main/java/com/albertkozak/chatee/MainActivity.kt
@@ -2,11 +2,17 @@ package com.albertkozak.chatee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("711 DA BEST")
     }
 }
